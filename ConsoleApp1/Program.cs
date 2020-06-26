58410cd4c0bb5127582c89a442b8c43bd21b80e7
dave.groves <microsoft@groverhome.co.uk>
2020-06-26 08:31:15 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+using static GrpcService1.Greeter;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GreeterClient(channel);
+
+            var request = new HelloRequest { Name = "Grover" };
+            var response = await client.SayHelloAsync(request);
+
+            Console.WriteLine(response.Message);
+
+            Console.ReadKey();
+        }
+    }
+}
